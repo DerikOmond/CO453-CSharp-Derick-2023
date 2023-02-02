3406c84e05366c746a46611d959364a006d37568
DerikOmond <derik.omond@gmail.com>
2023-02-02 13:47:54 +0000

    Miles to feet converter
    User Inputs miles and output of miles in feet is printed onto terminal.

--- ConsoleAppProject/App01/DistanceConverter.cs
+++ ConsoleAppProject/App01/DistanceConverter.cs
@@ -10,5 +10,23 @@ namespace ConsoleAppProject.App01
     /// </author>
     public class DistanceConverter
     {
+        public void Run()
+        {
+            //step1 - Input needed from the user
+
+            Console.Write("Enter no. of miles: ");
+           string input = Console.ReadLine(); 
+           int miles = Convert.ToInt32(input);
+           Console.WriteLine("Miles entered: " +miles);
+
+           //step 2 - convert miles into feet
+
+            const int MILES_TO_FEET = 5280;
+           int feet = miles*MILES_TO_FEET;
+
+           //step 3 - print feet
+
+           Console.WriteLine(miles + " miles is " + feet + " feet.");
+        }
     }
 }

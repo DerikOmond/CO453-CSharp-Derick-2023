ad590fc16c59e997be0bd960c38df81e91059a9d
DerikOmond <derik.omond@gmail.com>
2023-02-09 17:28:11 +0000

    Restarting
    Beginning from the start in order to simplify the code by using methods more often.

--- ConsoleAppProject/App01/DistanceConverter.cs
+++ ConsoleAppProject/App01/DistanceConverter.cs
@@ -10,38 +10,5 @@ namespace ConsoleAppProject.App01
     /// </author>
     public class DistanceConverter
     {
-        public void Run()
-        {
-            //Display heading information
-
-            Console.WriteLine("This console application allows you to convert from one distance unit to another.\nWould you like to try?: ");
-            String userAccept = Console.ReadLine();
-
-            if(userAccept.Equals("yes"))
-            {
-                Console.WriteLine("You agreed!!\n");
-            }
-            else
-            {
-                Console.WriteLine("Oh well :)");
-            }
-
-            //step1 - Input needed from the user
-
-            Console.Write("Enter no. of miles: ");
-           string input = Console.ReadLine(); 
-           int miles = Convert.ToInt32(input);
-           Console.WriteLine("Miles entered: " +miles);
-
-           //step 2 - convert miles into feet
-
-            const int MILES_TO_FEET = 5280;
-           int feet = miles*MILES_TO_FEET;
-
-           //step 3 - print feet
-
-           Console.WriteLine(miles + " miles is " + feet + " feet.");
-            Console.ReadLine();
-        }
     }
 }

f1e63252effce7340f3120cc6d1b034ef5f735b5
DerikOmond <derik.omond@gmail.com>
2023-02-09 17:56:19 +0000

    Heading
    Method to display the heading of what the program does.

--- ConsoleAppProject/App01/DistanceConverter.cs
+++ ConsoleAppProject/App01/DistanceConverter.cs
@@ -3,12 +3,29 @@
 namespace ConsoleAppProject.App01
 {
     /// <summary>
-    /// Please describe the main features of this App
+    /// This console app allows users to convert between units of distance
     /// </summary>
     /// <author>
-    /// Derek version 0.1
+    /// Derick Omondi Version 0.1
     /// </author>
     public class DistanceConverter
     {
+        /// <running>
+        /// In order to begin the process of converting units of distances, the 'Run'
+        /// method will be called as a way of calling the other methods relevent to
+        /// the class.
+        /// </running>
+        public void Run()
+        {
+            Heading();
+            Console.ReadLine();
+        }
+
+        public void Heading()
+        {
+            Console.WriteLine("===========================================================================================");
+            Console.WriteLine("-----------This console app allows you to convert between units of measurements.-----------");
+            Console.WriteLine("===========================================================================================");
+        }
     }
 }
